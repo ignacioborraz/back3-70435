8fc78cfa0e378c2b102676b275735a0609f451bb
ignacioborraz <ignacioborraz@hotmail.com>
2025-04-10 11:53:19 -0300

    argsv+env

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import "dotenv/config.js";
+import "./src/helpers/env.js"
 import express from "express";
 import dbConnect from "./src/helpers/dbConnect.helper.js";
 import indexRouter from "./src/routers/index.router.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
+import argvs from "./src/helpers/arguments.helper.js"
 
 /* server settings */
 const server = express();
 const port = process.env.PORT || 8080;
 const ready = async () => {
-  console.log("server ready on port " + port);
+  console.log("server ready on port " + port+" and mode "+ argvs.mode);
   await dbConnect(process.env.LINK_DB);
 };
 server.listen(port, ready);
@@ -21,3 +22,5 @@ server.use(express.static("public"));
 /* router settings */
 server.use("/", indexRouter);
 server.use(pathHandler);
+
+//console.log(process.argv);
