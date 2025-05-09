2ad6ab4448f39e3b7b923d4b98266a1e54ee3c7a
ignacioborraz <ignacioborraz@hotmail.com>
2025-05-09 10:42:21 -0300

    dockerf

---
+++ dockerfile
@@ -0,0 +1,16 @@
+# indicamos la imagen base del proyecto
+FROM node
+
+# establecemos el nombre de la app / directorio de trabajo
+WORKDIR /coderserver-70435
+
+# copiamos los archivos de la aplicación al contenedor
+COPY package.json ./
+RUN npm install
+COPY . .
+
+# exponemos el puerto de la aplicacion
+EXPOSE 9000
+
+# definimos el comando para ejecutar la app
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "./src/helpers/env.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import compression from "express-compression";
+import cluster from "cluster";
+import { cpus } from "os";
 import dbConnect from "./src/helpers/dbConnect.helper.js";
 import indexRouter from "./src/routers/index.router.js";
 import winston from "./src/middlewares/winston.mid.js";
@@ -12,20 +14,31 @@ import logger from "./src/helpers/logger.helper.js";
 
 /* server settings */
 const server = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const ready = async () => {
   // a partir de ahora REGISTRO LOGS unicamente con winston!!!
   //console.log("server ready on port " + port + " and mode " + argvs.mode);
-  logger.INFO("server ready on port " + port + " and mode " + argvs.mode)
+  logger.INFO("server ready on port " + port + " and mode " + argvs.mode);
+  logger.INFO("server ready on pid " + process.pid);
   await dbConnect(process.env.LINK_DB);
 };
-server.listen(port, ready);
+const isPrimary = cluster.isPrimary;
+if (isPrimary) {
+  /* si estoy en un proceso primario (primary) voy a crear los hijos (workers) */
+  const numberOfProcess = cpus().length;
+  for (let index = 1; index <= numberOfProcess; index++) {
+    cluster.fork();
+  }
+} else {
+  /* si estoy en un worker voy a levantar un servidor, "PRENDIENDO" un nodo del cluster */
+  server.listen(port, ready);
+}
 
 /* middlewares settings */
 server.use(compression({ brotli: { enabled: true, zlib: {} } }));
 server.use(cookieParser());
 server.use(express.json());
-server.use(winston)
+server.use(winston);
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
 
